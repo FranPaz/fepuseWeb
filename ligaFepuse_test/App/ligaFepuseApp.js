18019e9117a6aaeb6eca255722183148cf0045d7
kikexp <enriquepaz.inbox@hotmail.com>
2016-01-12 17:30:02 -0300

    ligaFepuse_test

---
+++ ligaFepuse_test/App/ligaFepuseApp.js
@@ -0,0 +1,24 @@
+﻿var vLaboralApp = angular.module('vLaboralApp', ['ngResource', 'ngMdIcons', 'ui.router', 'ngCookies', 'ngTable',
+  'ngSanitize', 'ngAnimate', 'ngAria', 'ct.ui.router.extras', 'angular-loading-bar', 'daypilot', 'LocalStorageModule', 'angular-jwt', 'ngMaterial', 
+  'oc.lazyLoad', 'ng-mfb', 'ngAutocomplete', 'angular-input-stars'])
+    .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $stickyStateProvider, cfpLoadingBarProvider) {
+        $stateProvider
+        .state('equipos', {
+            abstract: true,
+            url: '/equipos',
+            views: {
+                '': {
+                    templateUrl: ''
+                },
+                'aside': {
+                    templateUrl: ''
+                },
+                'content': {
+                    templateUrl: ''
+                }
+            }
+        })
+
+        .state('equipos.carga')
+
+    })
